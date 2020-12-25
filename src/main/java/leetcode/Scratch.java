00aeb0094ee5c06127f71d79381e997b2fa46eae
wenjun6 <Axdi9310415a!>
2020-12-26 00:51:50 +0800

    配置项目为java

--- src/main/java/leetcode/Scratch.java
+++ src/main/java/leetcode/Scratch.java
@@ -1,6 +1,32 @@
+package leetcode;
+
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ *假定需要速记的内容记作字符串 word，速记员小明遵循特殊的缩写规则：连续子串将可缩写为其长度。注意：
+ * 1). 小明可选择内容是否被缩写；
+ * 2). 小明可选择内容中共有几处被缩写，但每两处缩写不能相邻。
+ *
+ * 请返回字符串 word 的缩写之后长度为 limit 的所有方案。
+ *
+ * 注意：输出的顺序并不重要。
+ *
+ * 示例 1：
+ *
+ * 输入: word = "hello", limit = 3
+ * 输出: ["3l1","3lo","2l2","1e3","h3o","he3"]
+ *
+ * 示例 2：
+ *
+ * 输入: word = "sea", limit = 3
+ * 输出: ["1e1","1ea","s1a","se1","sea"]
+ *
+ * 提示：
+ *
+ * 1 <= word.length <= 15 且仅包含小写字符
+ * 1 <= limit <= word.length
+ */
 class Scratch {
     public static void main(String[] args) {
         String word = "hello";
